ddefbf7e691a41776ec4b31d9475e7c483769864
Jonathan Monsonego <jomonson@microsoft.com>
2017-09-11 10:49:38 +0300

    done

---
+++ OwinCompressionDependencyIssue/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Owin;
+
+namespace OwinCompressionDependencyIssue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (OwinCompression.basePath == "")
+            {
+                Console.WriteLine("aasc");
+            }
+        }
+    }
+}
